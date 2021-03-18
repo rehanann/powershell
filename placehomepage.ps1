1839e9013c41a42897248144f28eacacbdc952ae
rehanann <rehanann@gmail.com>
2021-03-18 13:21:05 +0000

    add ps2 with recommanded comments

--- placehomepage.ps1
+++ placehomepage.ps1
@@ -8,7 +8,7 @@ $header = @"
 "@
 
 #Install IIS
-#Install-WindowsFeature -name Web-Server
+Install-WindowsFeature -name Web-Server
 
 #Query Azure Instance Metadata service
 $metadata = Invoke-RestMethod -Headers @{"Metadata"="true"} -URI http://169.254.169.254/metadata/instance?api-version=2017-08-01 -Method get
@@ -21,5 +21,6 @@ $body.Add([pscustomobject] @{"Heading" = "Dilemna"; "Data" = "schrodinger's cat 
 # $body.Add([pscustomobject] @{"Heading" = "Private IP"; "Data" = $metadata.network.interface.ipv4.ipAddress.privateIpAddress}) | Out-Null
 
 #Convert object to HTML and overwrite existing index
-$HTML = $body | ConvertTo-Html -Title "WebServer Details"-Head $header
+# $HTML = $body | ConvertTo-Html -Title "WebServer Details"-Head $header
+$HTML = ConvertTo-Html -Fragment "<h3>schrodinger's cat is alive</h3>" -Title "WebServer Details"-Head $header
 $HTML | Out-File -FilePath "C:\inetpub\wwwroot\index.html" -Force
